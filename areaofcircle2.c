bf8720c1a5c55d4dfb958aed18c62f5abfe3198f
Nathan Duggal <ndugga1001@gmail.com>
2021-01-12 21:07:38 +0000

    updating areaofcircle2.c

--- areaofcircle2.c
+++ areaofcircle2.c
@@ -15,44 +15,51 @@ void getTestInput(int argc, char* argv[], float* a, float* b)
 // add your areaOfCircle function here - it must NOT printf, instead it must
 // return the result to be printed in main
 
+
+static float start;
+static float end;
+static char input[256];
+
 double areaOfCircle (float rad){
   double area = rad*rad*M_PI;
   return area;
 }
 
+void getVals(){
+  while(1){
+    printf("Start:\n");
+    while(1){
+      fgets(input, 256, stdin);
+      if((sscanf(input,"%f", &start) == 1) && (start > 0)){
+        break;
+      }
+      printf("Invalid start\n");
+    }
+    printf("End:\n");
+    while(1){
+      fgets(input, 256, stdin);
+      if((sscanf(input,"%f", &end) == 1) && (end > 0)){
+        break;
+      }
+      printf("Invalid end\n");
+    }
+    if(start <= end) break;
+    printf("%f is not greater than or equal to %f \n", start, end);
+  }
+}
+
 int main(int argc, char* argv[]) 
 {
   // the two variables which control the number of times areaOfCircle is called
-  char input1[256];
-  char input2[256];
-  float start;
-  float end;
-  if(argc > 1){
+
+  if(argc == 3){
     sscanf(argv[1], "%f", &start);
     sscanf(argv[2], "%f", &end);
-  }else{
-    while(1){
-      printf("Start:\n");
-      while(1){
-        fgets(input1, 256, stdin);
-        if((sscanf(input1,"%f", &start) == 1) && (start > 0)){
-          printf("%f\n",start);
-          break;
-        }
-        printf("Invalid start\n");
-      }
-      printf("End:\n");
-      while(1){
-        fgets(input2, 256, stdin);
-        if((sscanf(input2,"%f", &end) == 1) && (end > 0)){
-          printf("%f\n",end);
-          break;
-        }
-        printf("Invalid end\n");
-      }
-      if(start <= end) break;
-      printf("%f is not greater than or equal to %f \n", start, end);
+    if((start < 0) || (end < 0) || (start > end)){
+      getVals();
     }
+  }else{
+    getVals();
   }
   // for testing only - do not change
   getTestInput(argc, argv, &start, &end);
